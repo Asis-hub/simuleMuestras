1fcb8d4415f6b886ee09144ca6650cf0cde216d2
Asis-hub <asis.reyes@hotmail.com>
2023-05-19 13:19:03 -0500

    Refactorización vista de calculo de encuestadores

---
+++ resources/views/encuestadores/form.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('title', $viewData['title'])
+@section('content')
+    <div class="row">
+        <div class="col-md-6 col-lg-4 mb-2">
+            Fila 1 - Placeholder
+        </div>
+        <div class="col-md-6 col-lg-4 mb-2">
+            Fila 2 - Placeholder
+        </div>
+        <div class="col-md-6 col-lg-4 mb-2">
+            Fila 3 - Placeholder
+        </div>
+        <div class="col-md-6 col-lg-4 mb-2">
+            Fila 4 - Placeholder
+        </div>
+    </div>
+@endsection
