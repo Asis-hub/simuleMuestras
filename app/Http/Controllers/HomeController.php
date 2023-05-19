e62b07c50b7c9380e2811821bd1880ef7fe3aad6
Asis-hub <asis.reyes@hotmail.com>
2023-05-19 12:36:44 -0500

    Estándar de codificación (PSR-2) y code sniffer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,21 +1,23 @@
 <?php
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
-public function index()
-{
-$viewData = [];
-$viewData["title"] = "SIMULE - Centro de Estudios de Opinión y Análisis";
-return view('home.index')->with("viewData", $viewData);
-}
-public function about()
-{
-    $viewData = [];
-    $viewData["title"] = "Acerca de - Centro de Estudios de Opinión y Análisis";
-    $viewData["subtitle"] = "Acerca de";
-    $viewData["description"] = "Universidad Veracruzana ...";
-    $viewData["author"] = "CEOA";
-    return view('home.about')->with("viewData", $viewData);
-}
+    public function index()
+    {
+        $viewData = [];
+        $viewData["title"] = "SIMULE - Centro de Estudios de Opinión y Análisis";
+        return view('home.index')->with("viewData", $viewData);
+    }
+    public function about()
+    {
+        $viewData = [];
+        $viewData["title"] = "Acerca de - Centro de Estudios de Opinión y Análisis";
+        $viewData["subtitle"] = "Acerca de";
+        $viewData["description"] = "Universidad Veracruzana ...";
+        $viewData["author"] = "CEOA";
+        return view('home.about')->with("viewData", $viewData);
+    }
 }
