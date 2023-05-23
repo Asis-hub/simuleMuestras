e10ceb628b90da3a3b1ef0cd72daf7102e314da0
Asis-hub <asis.reyes@hotmail.com>
2023-05-23 16:30:07 -0500

    Migración de BD para manejo de roles de usuario.

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-
 class AdminUserController extends Controller
 {
     public function index()
--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,18 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
+    * USER ATTRIBUTES
+    * $this->attributes['id'] - int - contains the user primary key (id)
+    * $this->attributes['name'] - string - contains the user name* $this->attributes['email'] - string - contains the user email
+    * $this->attributes['email_verified_at'] - timestamp - contains the user email verification date
+    * $this->attributes['password'] - string - contains the user password
+    * $this->attributes['remember_token'] - string - contains the user password
+    * $this->attributes['role'] - string - contains the user role (auxiliar, admin o coordinador)
+    * $this->attributes['created_at'] - timestamp - contains the user creation date
+    * $this->attributes['updated_at'] - timestamp - contains the user update date
+    */
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
@@ -73,5 +85,29 @@ class User extends Authenticatable
     public function setPassword($password)
     {
         $this->attributes['password'] = $password;
+    }
+    public function getRole()
+    {
+        return $this->attributes['role'];
+    }
+    public function setRole($role)
+    {
+        $this->attributes['role'] = $role;
+    }
+    public function getCreatedAt()
+    {
+        return $this->attributes['created_at'];
+    }
+    public function setCreatedAt($createdAt)
+    {
+        $this->attributes['created_at'] = $createdAt;
+    }
+    public function getUpdatedAt()
+    {
+        return $this->attributes['updated_at'];
+    }
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->attributes['updated_at'] = $updatedAt;
     }
 }
