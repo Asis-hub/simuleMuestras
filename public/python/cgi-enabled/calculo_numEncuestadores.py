e8c3bf6af1b763c14943f858c33805eb67129239
Asis-hub <asis.reyes@hotmail.com>
2023-06-22 12:37:18 -0600

    for loop redondear encuestas

--- public/python/cgi-enabled/calculo_numEncuestadores.py
+++ public/python/cgi-enabled/calculo_numEncuestadores.py
@@ -7,6 +7,7 @@ print("Content-type: text/html\n")
 import cgi
 import json
 import numpy as np
+import math
 
 form = cgi.FieldStorage()
 
@@ -23,16 +24,25 @@ inputncuestadores_Hombres_50_64 = float(form["encuestadores_Hombres_50_64_py"].v
 inputncuestadores_Mujeres_65 = float(form["encuestadores_Mujeres_65_py"].value)
 inputncuestadores_Hombres_65 = float(form["encuestadores_Hombres_65_py"].value)
 
-resultado_inputncuestadores_Mujeres_18_24 = int(np.floor(inputncuestadores_Mujeres_18_24 * numEncuestadores))
-resultado_inputncuestadores_Hombres_18_24 = int(np.floor(inputncuestadores_Hombres_18_24 * numEncuestadores))
-resultado_inputncuestadores_Mujeres_25_34 = int(np.floor(inputncuestadores_Mujeres_25_34 * numEncuestadores))
-resultado_inputncuestadores_Hombres_25_34 = int(np.floor(inputncuestadores_Hombres_25_34 * numEncuestadores))
-resultado_inputncuestadores_Mujeres_35_49 = int(np.floor(inputncuestadores_Mujeres_35_49 * numEncuestadores))
-resultado_inputncuestadores_Hombres_35_49 = int(np.floor(inputncuestadores_Hombres_35_49 * numEncuestadores))
-resultado_inputncuestadores_Mujeres_50_64 = int(np.floor(inputncuestadores_Mujeres_50_64 * numEncuestadores))
-resultado_inputncuestadores_Hombres_50_64 = int(np.floor(inputncuestadores_Hombres_50_64 * numEncuestadores))
-resultado_inputncuestadores_Mujeres_65 = int(np.floor(inputncuestadores_Mujeres_65 * numEncuestadores))
-resultado_inputncuestadores_Hombres_65 = int(np.floor(inputncuestadores_Hombres_65 * numEncuestadores))
+import math
+
+def process_value(val):
+    if (float(val) % 1) >= 0.5:
+        x = math.ceil(val)
+    else:
+        x = round(val)
+    return x
+
+resultado_inputncuestadores_Mujeres_18_24 = process_value(inputncuestadores_Mujeres_18_24 * numEncuestadores)
+resultado_inputncuestadores_Hombres_18_24 = process_value(inputncuestadores_Hombres_18_24 * numEncuestadores)
+resultado_inputncuestadores_Mujeres_25_34 = process_value(inputncuestadores_Mujeres_25_34 * numEncuestadores)
+resultado_inputncuestadores_Hombres_25_34 = process_value(inputncuestadores_Hombres_25_34 * numEncuestadores)
+resultado_inputncuestadores_Mujeres_35_49 = process_value(inputncuestadores_Mujeres_35_49 * numEncuestadores)
+resultado_inputncuestadores_Hombres_35_49 = process_value(inputncuestadores_Hombres_35_49 * numEncuestadores)
+resultado_inputncuestadores_Mujeres_50_64 = process_value(inputncuestadores_Mujeres_50_64 * numEncuestadores)
+resultado_inputncuestadores_Hombres_50_64 = process_value(inputncuestadores_Hombres_50_64 * numEncuestadores)
+resultado_inputncuestadores_Mujeres_65 = process_value(inputncuestadores_Mujeres_65 * numEncuestadores)
+resultado_inputncuestadores_Hombres_65 = process_value(inputncuestadores_Hombres_65 * numEncuestadores)
 
 resultado = [resultado_inputncuestadores_Mujeres_18_24, resultado_inputncuestadores_Hombres_18_24, resultado_inputncuestadores_Mujeres_25_34,
 resultado_inputncuestadores_Hombres_25_34, resultado_inputncuestadores_Mujeres_35_49, resultado_inputncuestadores_Hombres_35_49,
